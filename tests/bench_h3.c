5d7b5303c2c5353cd4cc0ce2657421916b6f2f04
c9s <yoanlin93@gmail.com>
2014-06-21 18:44:37 +0800

    Add bench_h3 file for benchmarking

---
+++ tests/bench_h3.c
@@ -0,0 +1,13 @@
+/*
+ * bench_h3.c
+ * Copyright (C) 2014 c9s <c9s@c9smba.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "bench/bench.h"
+
+int main() {
+    return 0;
+}
+
